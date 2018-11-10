2a400499c62f5615a5a373037e8c9d5e2a65e495
Brandon Roehl <SkyGuy913@gmail.com>
2018-11-10 14:01:37 -0600

    What it is

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
+
+    rescue_from ActiveRecord::RecordNotFound do |message|
+        logger.error message
+        respond_to do |format|
+            format.json do
+                render json: {error: "404 record not found"}, status: :not_found
+            end
+            format.html do
+                raise ActionController::RoutingError.new('Not Found')
+            end
+        end
+    end
+
+    # No layout for ajax
+    layout proc{|c| c.request.xhr? ? false : "application"}
+
+    def splash
+        render "splash/index"
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
     devise_for :users, controllers: {
-        sessions: 'users/sessions'
+        registrations: 'users/registrations'
     }
+
+    root to: 'application#splash'
 
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
