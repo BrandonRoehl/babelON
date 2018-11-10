db5eacd455178ff9703731aa584a4d305ac8bb1d
Quinn Huettner <huettnqm4285@uwec.edu>
2018-11-10 13:26:29 -0600

    some initial view stuff

---
+++ babelON/contact.swift
@@ -0,0 +1,13 @@
+//
+//  contact.swift
+//  babelON
+//
+//  Created by Potato on 11/10/18.
+//  Copyright © 2018 Quinn Huettner. All rights reserved.
+//
+
+import UIKit
+
+class contact: NSObject {
+
+}
---
+++ babelON/user.swift
@@ -0,0 +1,13 @@
+//
+//  user.swift
+//  babelON
+//
+//  Created by Potato on 11/10/18.
+//  Copyright © 2018 Quinn Huettner. All rights reserved.
+//
+
+import UIKit
+
+class user: NSObject {
+
+}
