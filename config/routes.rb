5ec02bb666e3691f024909f41d65073f3f3fdad7
Brandon Roehl <SkyGuy913@gmail.com>
2018-11-10 15:45:40 -0600

    Routes and views ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  resources :localizations
-  resources :messages
     devise_for :users, controllers: {
         registrations: 'users/registrations'
     }
 
     root to: 'application#splash'
+
+    resources :localizations
+    resources :messages
+    resources :conversations
 
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
